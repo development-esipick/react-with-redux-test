8c25711cad573a9ac1cb3be649e3ccc33e11c670
Zohaib Tariq <se.zohaib@gmail.com>
2020-09-06 11:43:51 +0500

    fix rename of folder components so git can track it with perfection

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk'
 import React from 'react';
 
-import ContactListingsPortal from "./contactComponenets/contactModals/contactListingsPortal"
-import Main from "./contactComponenets/main/main";
+import ContactListingsPortal from "./componenets/contactModals/contactListingsPortal"
+import Main from "./componenets/main/main";
 import './index.scss'
 
 const store = createStore(AllReducers, composeWithDevTools(applyMiddleware(thunk)));
